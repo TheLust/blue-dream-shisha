3aa7d5e9a96eea60d59c6df0303743196d6d87d3
Daniel Poverjuc <daniel.poverjuc@iongroup.com>
2024-08-06 18:06:39 +0300

    Added MaterialUI to Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatDrawer, MatDrawerContainer, MatDrawerContent } from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatDrawerContainer,
+    MatDrawer,
+    MatDrawerContent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
